fd82a25d189a77bf4703310cad2689997052f73b
Felix Ahlcrona <felix.ahlcrona@hotmail.com>
2024-10-09 18:06:57 +0200

    pdf

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
                 </div>
 
                 <div class="animate2s">
-                  <a href="https://drive.google.com/file/d/1I5cQ0W2vrF5Yh8UbCb1cNuCsNKNPAgtv/view?usp=sharing" target="_blank">
+                  <a href="https://drive.google.com/file/d/1QdlxyXnItot3JORrOc2JPcxUdvxrHrpc/view?usp=sharing" target="_blank">
                     <button class="button is-large" id="button">
                       <span className="buttonText">Resumé</span>
                       <span class="icon is-medium" id="icon">
